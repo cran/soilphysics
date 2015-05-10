7d34b2c6b60125807943417e3bcc5da6a9ce1cac
Anderson Rodrigo da Silva <anderson.agro@hotmail.com>
2015-05-10 00:24:18 +0000

    version 2.3

--- R/iwc.R
+++ R/iwc.R
@@ -1,7 +1,18 @@
 iwc <- 
 function(theta_R, theta_S, alpha, n, a, b, hos = 0, 
-      graph = TRUE, xlab = "Matric potential (hPa)", 
-      ylab = "Water content", ...)
+      graph = TRUE, 
+	xlab = "Matric potential (hPa)", 
+      ylab = "Water content", 
+	xlim1 = NULL,
+	xlim2 = NULL, 
+	xlim3 = NULL,
+	ylim1 = NULL, 
+	ylim2 = NULL, 
+	ylim3 = NULL,
+	col12 = c("black", "blue", "red"),
+	col3 = c("orange", "black"),
+	lty12 = c(1, 3, 2), 
+	lty3 = c(2, 1), ...)
 {
     stopifnot(hos >= 0)
 
@@ -211,56 +222,91 @@ function(theta_R, theta_S, alpha, n, a, b, hos = 0,
     # -------------------------------------------------
     # graphic
     if (graph) {
-        curve(der(x, theta_R, theta_S, alpha, n), 
-            from = 0, to = 350, 
+        layout(matrix(c(1, 3, 2, 3), 2))
+        curve.(der(x, theta_R, theta_S, alpha, n), 
+            from = 1, to = 350,
+            xlim = xlim1,
+            ylim = ylim1, 
             xlab = xlab, ylab = ylab, 
-            main = "Wet range", ...)
-        curve(EK(x, theta_R, theta_S, alpha, n), 
-           add = TRUE, lty = 3, col = 4)
-        curve(EKa(x, theta_R, theta_S, alpha, n), 
-           add = TRUE, lty = 2, col = 2)
-        legend("topright",  c(expression(C(h)), 
-             expression(E[R](h)), expression(E[RKdry](h))), 
-             lty = c(1, 3, 2), col = c(1, 4, 2), 
-             bg = "lightyellow")
+            main = "Wet range",
+            col = col12[1],
+            lty = lty12[1], 
+            shade = col12[1], ...)
+        curve.(EK(x, theta_R, theta_S, alpha, n), 
+            add = TRUE, 
+            col = col12[2],
+            lty = lty12[2], 
+            shade = col12[2])
+        curve.(EKa(x, theta_R, theta_S, alpha, n), 
+            add = TRUE,
+            col = col12[3],
+            lty = lty12[3], 
+            shade = col12[3])
     
-        dev.new(width = 5, height = 5)
-        curve(der(x, theta_R, theta_S, alpha, n), 
-            from = h1.5, to = 15000, 
+        #dev.new(width = 5, height = 5)
+        curve.(der(x, theta_R, theta_S, alpha, n), 
+            from = h1.5, to = 15000,
+            xlim = xlim2,
+            ylim = ylim2, 
             xlab = xlab, ylab = ylab, 
-            main = "Dry range", ...)
-        curve(ER(x, a, b, theta_R, theta_S, alpha, n), 
+            main = "Dry range",
+            col = col12[1],
+            lty = lty12[1], 
+            shade = col12[1], ...)
+        curve.(ER(x, a, b, theta_R, theta_S, alpha, n), 
             add = TRUE, from = h1.5, to = h2.5., 
-            lty = 3, col = 4)
-        curve(ERKdry(x, a, b, d = coef(fit2)[2], 
+            col = col12[2],
+            lty = lty12[2], 
+            shade = col12[2])
+        curve.(ERKdry(x, a, b, d = coef(fit2)[2], 
             theta_R, theta_S, alpha, n), 
             add = TRUE, from = 12000, to = 15000, 
-            lty = 2, col = 2)
-        legend("topright",  c(expression(C(h)), 
-             expression(E[R](h)), expression(E[RKdry](h))), 
-             lty = c(1, 3, 2), col = c(1, 4, 2), 
+            col = col12[3],
+            lty = lty12[3], 
+            shade = col12[3])
+        legend("topright",  c(expression(C(h, h[os])), 
+             expression(E[R](h, h[os])), 
+             expression(E[RKdry](h, h[os]))), 
+             lty = lty12, col = col12, 
              bg = "lightyellow")
 
-        if (hos != 0) {
-           dev.new(width = 5, height = 5)
-           curve(der.(x, theta_R, theta_S, alpha, n), 
+        #if (hos != 0) {
+        #   dev.new(width = 5, height = 5)
+           curve.(der.(x, theta_R, theta_S, alpha, n), 
               from = 1, to = 15000, log = "x",
+              xlim = xlim3,
+              ylim = ylim3,
               xlab = xlab, ylab = ylab, 
-              main = "Salinity effect", ...)
-           curve(der(x, theta_R, theta_S, alpha, n), 
+              main = "Salinity effect",
+              col = col3[1],
+              lty = lty3[1], 
+              shade = col3[1], ...)
+           curve.(der(x, theta_R, theta_S, alpha, n), 
                add = TRUE, from = 1, to = 15000, 
-               log = "x", col = 2, lty = 2)
-           legend("topright", c(expression(C(h)), 
-               expression(E[S](h))), lty = c(1, 2), 
-               col = c(1, 2), bg = "lightyellow")
-        }        
+               log = "x",
+               col = col3[2],
+               lty = lty3[2], 
+               shade = col3[2])
+           legend("topright", c(expression(C(h, h[os]==0)), 
+               expression(C(h, h[os]))), 
+               lty = lty3, col = col3,
+               bg = "lightyellow")
+        #}        
     }
 
     # ----------------------------------------
     # output
-    IWC <- ave1 + ave2 + ave3 + ave4 + ave5
-    EI <- (ave1h + ave2h + ave3h + ave4h + ave5h) / (10 * IWC)
-    return(list(IWC = IWC, EI = EI))
+    IWC <- c(ave1, ave2, ave3, ave4, ave5, 0)
+    EI <- c(ave1h, ave2h, ave3h, ave4h, ave5h, 0) / (10 * sum(IWC))
+    li <- round(c(h10, h15, 330, h1.5, 12000, 0), 2)
+    ls <- round(c(h15, 330, h1.5, h2.5., 15000, 15000), 2)
+    h.Range <- paste(li, "-", ls)
+    mout <- data.frame(IWC = round(IWC, 4), 
+       EI = round(EI, 2), h.Range)
+    mout[6, 1:2] <- c(sum(IWC), sum(EI))
+    rownames(mout) <- c("EKa(h, hos)", "EK(h, hos)", "C(h, hos)", 
+       "ER(h, hos)", "ERKdry(h, hos)", "Sum")
+    return(mout)
 }
 
 
@@ -278,4 +324,76 @@ function(x, y)
    out <- 0.5 * sum((x[2:n] - x[2:n - 1]) * 
       (y[2:n] + y[2:n - 1]))
    return(out)
+}
+
+# --------------------------------------------
+# shaded curve()
+curve. <- 
+function (expr, from = NULL, to = NULL, n = 101, add = FALSE, 
+    type = "l", xname = "x", xlab = xname, ylab = NULL, log = NULL, 
+    xlim = NULL, shade = "black", ...) 
+{
+    sexpr <- substitute(expr)
+    if (is.name(sexpr)) {
+        expr <- call(as.character(sexpr), as.name(xname))
+    }
+    else {
+        if (!((is.call(sexpr) || is.expression(sexpr)) && xname %in% 
+            all.vars(sexpr))) 
+            stop(gettextf("'expr' must be a function, or a call or an expression containing '%s'", 
+                xname), domain = NA)
+        expr <- sexpr
+    }
+    if (dev.cur() == 1L && !identical(add, FALSE)) {
+        warning("'add' will be ignored as there is no existing plot")
+        add <- FALSE
+    }
+    addF <- identical(add, FALSE)
+    if (is.null(ylab)) 
+        ylab <- deparse(expr)
+    if (is.null(from) || is.null(to)) {
+        xl <- if (!is.null(xlim)) 
+            xlim
+        else if (!addF) {
+            pu <- par("usr")[1L:2L]
+            if (par("xaxs") == "r") 
+                pu <- extendrange(pu, f = -1/27)
+            if (par("xlog")) 
+                10^pu
+            else pu
+        }
+        else c(0, 1)
+        if (is.null(from)) 
+            from <- xl[1L]
+        if (is.null(to)) 
+            to <- xl[2L]
+    }
+    lg <- if (length(log)) 
+        log
+    else if (!addF && par("xlog")) 
+        "x"
+    else ""
+    if (length(lg) == 0) 
+        lg <- ""
+    if (grepl("x", lg, fixed = TRUE)) {
+        if (from <= 0 || to <= 0) 
+            stop("'from' and 'to' must be > 0 with log=\"x\"")
+        x <- exp(seq.int(log(from), log(to), length.out = n))
+    }
+    else x <- seq.int(from, to, length.out = n)
+    ll <- list(x = x)
+    names(ll) <- xname
+    y <- eval(expr, envir = ll, enclos = parent.frame())
+    if (length(y) != length(x)) 
+        stop("'expr' did not evaluate to an object of length 'n'")
+    if (isTRUE(add)) 
+        lines(x = x, y = y, type = type, ...)
+    else plot(x = x, y = y, type = type, xlab = xlab, ylab = ylab, 
+        xlim = xlim, log = lg, ...)
+    x. <- c(min(x), x, max(x))
+    y. <- c(0, y, 0)
+    polygon(x = x., y = y., 
+       border = NA,
+       col = adjustcolor(shade, alpha.f = 0.1))
+    invisible(list(x = x, y = y))
 }
